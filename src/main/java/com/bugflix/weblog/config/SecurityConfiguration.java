a62058f4003865d36ee477c371130c0d3ec1a371
Jang-namu <ironhee8005@gmail.com>
2024-02-14 18:48:29 +0900

    Revert "feat: SecurityConfig CORS 모든 오리진 허용"
    This reverts commit 8bcb90c19e2ace3221ca7f78adf587f463c250ed.

--- src/main/java/com/bugflix/weblog/config/SecurityConfiguration.java
+++ src/main/java/com/bugflix/weblog/config/SecurityConfiguration.java
@@ -109,9 +109,6 @@ public class SecurityConfiguration {
         // Cors 허용 패턴
         configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("*"));
-        configuration.addAllowedOrigin("*");
-        configuration.addAllowedHeader("*");
-        configuration.addAllowedMethod("*");
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
