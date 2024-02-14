743e5f8ca85899cd064943694ab3582b95d5571b
Jang-namu <ironhee8005@gmail.com>
2024-02-14 18:48:29 +0900

    Revert "remove: 실제 사용되지 않는 DTO 삭제"
    This reverts commit 9b50001a185460ddd60d2778b6150128daec8202.

---
+++ src/main/java/com/bugflix/weblog/tag/dto/TagRequest.java
@@ -0,0 +1,11 @@
+package com.bugflix.weblog.tag.dto;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class TagRequest {
+    private String tag;
+}
