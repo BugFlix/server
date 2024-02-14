1347ffad4c3277fbccb95906178444f5cc9dd927
Jang-namu <ironhee8005@gmail.com>
2024-02-14 18:48:29 +0900

    Revert "delete-logic"
    This reverts commit a90f665e80300725a0952bda577b59def899331f.

--- src/main/java/com/bugflix/weblog/post/PostRepository.java
+++ src/main/java/com/bugflix/weblog/post/PostRepository.java
@@ -7,7 +7,6 @@ import java.util.List;
 public interface PostRepository extends JpaRepository<Post,Long> {
 
     public List<Post> findByPageUrl(String url);
-    public List<Post> findByPageUrlAndUserUserId(String url,Long userId);
-    public void deleteByPostId(Long postId);
 
+    public List<Post> findByPageUrlAndUserUserId(String url,Long userId);
 }
