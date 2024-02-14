8681792b399b52dc0ccf02c4f9c10060f467e165
Jang-namu <ironhee8005@gmail.com>
2024-02-14 18:48:29 +0900

    Revert "create getposts"
    This reverts commit c1e229771e47fd7bb52fa8a9b6987aa35e04aabc.

--- src/main/java/post/PostService.java
+++ src/main/java/post/PostService.java
@@ -1,17 +1,7 @@
 package post;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import post.dto.PostRequest;
 
 @Service
-@RequiredArgsConstructor
-@Slf4j
 public class PostService {
-    private final PostRepository postRepository;
-
-    public Boolean savePost(PostRequest postRequest){
-        return true;
-    }
 }
--- src/main/java/post/dto/PostRequest.java
+++ src/main/java/post/dto/PostRequest.java
@@ -4,7 +4,5 @@ import lombok.Data;
 
 @Data
 public class PostRequest {
-    private String title;
-    private String content;
-    private Long like;
+
 }
