c7c80b2611c99e8bc534cba6a45a0417afb3a518
Jang-namu <ironhee8005@gmail.com>
2024-02-14 18:48:29 +0900

    Revert "feat: refreshToken early version - 로그인 시, 액세스 토큰 및 리프레쉬 토큰 발급 - 리프레쉬 토큰을 통한 토큰 재발급"
    This reverts commit e8240f9d66925444805395a1c120770fd8caadf8.

--- src/main/java/com/bugflix/weblog/security/JwtProvider.java
+++ src/main/java/com/bugflix/weblog/security/JwtProvider.java
@@ -1,10 +1,7 @@
 package com.bugflix.weblog.security;
 
 
-import com.bugflix.weblog.security.domain.RefreshToken;
-import com.bugflix.weblog.security.repository.RefreshTokenRepository;
-import com.bugflix.weblog.security.service.CustomUserDetailServiceImpl;
-import com.bugflix.weblog.user.domain.Authority;
+import com.bugflix.weblog.user.Authority;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
@@ -27,9 +24,6 @@ import java.util.List;
 @Component
 @RequiredArgsConstructor
 public class JwtProvider {
-
-    private final RefreshTokenRepository refreshTokenRepository;
-
     private final CustomUserDetailServiceImpl customUserDetailService;
 
     @Value("${springboot.jwt.secret.key}")
@@ -37,11 +31,7 @@ public class JwtProvider {
 
     private Key secretKey;
 
-    @Value("${springboot.jwt.token.access-expiration-time}")
-    private long accessExpirationTime;
-
-    @Value("${springboot.jwt.token.refresh-expiration-time}")
-    private long refreshExpirationTime;
+    private final long expirationTime = 1000L * 60 * 60;
 
     @PostConstruct
     protected void init() {
@@ -49,10 +39,11 @@ public class JwtProvider {
     }
 
 
-    public String createAccessToken(String account, List<Authority> roles) {
+    public String createToken(String account, List<Authority> roles) {
+
         Claims claims = Jwts.claims()
                 .subject(account)
-                .add("roles", roles)
+                .add("roles",roles)
                 .build();
 
         Date now = new Date();
@@ -60,41 +51,21 @@ public class JwtProvider {
         String token = Jwts.builder()
                 .claims(claims)
                 .issuedAt(now)
-                .expiration(new Date(now.getTime() + accessExpirationTime))
+                .expiration(new Date(now.getTime () + expirationTime))
                 .signWith(secretKey)
                 .compact();
 
         return token;
     }
 
-    public String createRefreshToken(String account, List<Authority> roles) {
-        Claims claims = Jwts.claims()
-                .subject(account)
-                .add("roles", roles)
-                .build();
-
-        Date now = new Date();
-
-        String refreshToken = Jwts.builder()
-                .claims(claims)
-                .issuedAt(now)
-                .expiration(new Date(now.getTime() + refreshExpirationTime))
-                .signWith(secretKey)
-                .compact();
-
-        // redis에 저장, redisTemplate과 CrudRepository의 저장방식이 다른가보다.
-        // 조회는 Repository를 통해 수행하므로 저장도 repository로 저장
-        refreshTokenRepository.save(new RefreshToken(account, refreshToken, refreshExpirationTime));
-        return refreshToken;
-    }
-
     @Transactional(readOnly = true)
     public Authentication getAuthentication(String token) {
-        UserDetails userDetails = customUserDetailService.loadUserByUsername(getAccount(token));
+        UserDetails userDetails = customUserDetailService.loadUserByUsername(this.getAccount(token));
+
         return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
     }
 
-    public String getAccount(String token) {
+    private String getAccount(String token) {
         return Jwts.parser()
                 .setSigningKey(secretKey)
                 .build()
@@ -104,20 +75,16 @@ public class JwtProvider {
     }
 
     public String resolveToken(HttpServletRequest request) {
-        String token = request.getHeader("Authorization");
-        try {
-            if (!token.substring(0, "BEARER ".length()).equalsIgnoreCase("Bearer ")) {
-                throw new IllegalStateException("Token 정보가 존재하지 않음");
-            }
-            token = token.split(" ")[1].trim();
-        } catch (Exception e) {
-            return null;
-        }
-        return token;
+        return request.getHeader("Authorization");
     }
 
     public boolean validateToken(String token) {
         try {
+            if (!token.substring(0, "BEARER ".length()).equalsIgnoreCase("Bearer ")) {
+                return false;
+            }
+
+            token = token.split(" ")[1].trim();
             Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).build().parseClaimsJws(token);
             return !claims.getBody().getExpiration().before(new Date());
         } catch (Exception e) {
--- src/main/java/com/bugflix/weblog/security/repository/RefreshTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bugflix.weblog.security.repository;
-
-import com.bugflix.weblog.security.domain.RefreshToken;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
-}
